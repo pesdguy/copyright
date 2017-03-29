3620816e17d847b0027a373bd143e253a16bce65
shayBarel <sbarelozana@gmail.com>
2017-03-29 23:40:45 +0300

    Merge branch 'master' of https://github.com/pesdguy/copyright into shay_second_log_in
    # Conflicts:
    #	README.md
    #	app.js
    #	package.json
    #	routes/index.js
    #	views/execute.jade

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ try {
 }
 routes.init(config);
 
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
