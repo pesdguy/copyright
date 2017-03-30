27d0aa0510760feb00b27c2e6c463ab4dc87179b
shayBarel <sbarelozana@gmail.com>
2017-03-30 06:06:50 +0300

    Merge branch 'master' of https://github.com/pesdguy/copyright into shay_second_log_in
    # Conflicts:
    #	README.md
    #	app.js
    #	package.json
    #	routes/index.js
    #	views/execute.jade

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(bodyParser.urlencoded({
     extended: false
